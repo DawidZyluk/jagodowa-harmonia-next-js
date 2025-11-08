020d6589b4a536f48a3c66a59b62fba035439d23
DawidZyluk <dawid@typoedition.com>
2025-11-08 13:42:06 +0100

    Add framer-motion and motion dependencies, enhance HeaderSection with parallax effect, and update styles for improved layout and responsiveness. Include new background and foreground assets.

--- app/sections/HeaderSection/HeaderSection.tsx
+++ app/sections/HeaderSection/HeaderSection.tsx
@@ -1,23 +1,59 @@
+"use client";
+
 import Button from "@/app/components/Button/Button";
+import { motion, MotionValue, useScroll, useTransform } from "motion/react";
+import { useRef } from "react";
 import { createBemClassNames } from "../../utils";
 import "./HeaderSection.scss";
 
+function useParallax(value: MotionValue<number>, distance: number) {
+  return useTransform(value, [0, 1], [distance, -distance]);
+}
+
 export default function HeaderSection() {
+  const ref = useRef<HTMLElement | null>(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "end start"],
+  });
+
   const classNames = createBemClassNames(
     "header-section",
     undefined,
     "background",
+    "foreground",
+    "overlay",
     "content",
     "title",
     "description"
   );
 
+  const backgroundY = useParallax(scrollYProgress, -500);
+
   return (
-    <section className={classNames["header-section"]}>
-      <div className={classNames.background}></div>
+    <section className={classNames["header-section"]} ref={ref}>
+      <motion.div
+        aria-hidden
+        className={classNames.background}
+        style={{ y: backgroundY }}
+      />
+      <motion.img
+        aria-hidden
+        src="/foreground.svg"
+        alt=""
+        className={classNames.foreground}
+        style={{ top: '250px' }}
+      />
+      <div className={classNames.overlay} aria-hidden />
+
       <div className={classNames.content}>
-        <h1 className={classNames.title}>Cześć, jestem Jagoda - dietetyczka z własną historią.</h1>
-        <p className={classNames.description}>Tworzę smaczne i skuteczne plany żywieniowe, które pomagają polepszyć wyniki badań, zwiększyć energię i poprawić samopoczucie.</p>
+        <h1 className={classNames.title}>
+          Cześć, jestem Jagoda - dietetyczka z własną historią.
+        </h1>
+        <p className={classNames.description}>
+          Tworzę smaczne i skuteczne plany żywieniowe, które pomagają polepszyć
+          wyniki badań, zwiększyć energię i poprawić samopoczucie.
+        </p>
         <Button>Umów wizytę</Button>
       </div>
     </section>
