6f0d9d094dac4ea89a69a8be2b2be044c3f1d94c
Jagoda <giedrojc.jagoda@gmail.com>
2026-01-16 13:45:23 +0100

    tag manager v3

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,13 @@ import Script from "next/script";
 import "./globals.scss";
 import Navbar from "./components/Navbar/Navbar";
 
+
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700", "800"],
   variable: "--font-poppins",
 });
+
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://jagodowa-harmonia.pl"),
@@ -133,30 +135,14 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="pl">
       <head>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-ZS76T7RG4S"
-          strategy="afterInteractive"
-        />
-        <Script id="ga4-init" strategy="afterInteractive">
-          {`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-ZS76T7RG4S');
-    `}
-        </Script>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-GR4QYB63ZG"
-          strategy="afterInteractive"
-        />
-        <Script id="ga4-init" strategy="afterInteractive">
-          {`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-GR4QYB63ZG');
-    `}
-        </Script>
+        {/* GOOGLE TAG MANAGER */}
+    <Script id="gtm-head" strategy="afterInteractive">
+      {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-XXXXXXX');`}
+    </Script>
         <Script
           id="mailerlite-universal"
           strategy="beforeInteractive"
@@ -173,6 +159,20 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       </head>
 
       <body className={poppins.variable}>
+        {/* GTM noscript */}
+    <noscript>
+      <iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
+        height="0"
+        width="0"
+        style={{ display: "none", visibility: "hidden" }}
+      />
+    </noscript>
+
+    {/* schema.org */}
+    <Script id="org-schema" type="application/ld+json" strategy="afterInteractive">
+      {JSON.stringify(organizationJsonLd)}
+    </Script>
         <Script id="org-schema" type="application/ld+json" strategy="afterInteractive">
           {JSON.stringify(organizationJsonLd)}
         </Script>
