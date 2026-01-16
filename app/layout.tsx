c6de113f37672866c833b3d27b028ae5af52cc1b
Jagoda <giedrojc.jagoda@gmail.com>
2026-01-16 14:17:59 +0100

    revert

--- app/layout.tsx
+++ app/layout.tsx
@@ -138,10 +138,10 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         {/* GOOGLE TAG MANAGER */}
     <Script id="gtm-head" strategy="afterInteractive">
       {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-W2FGRMJ9');`}
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-XXXXXXX');`}
     </Script>
         <Script
           id="mailerlite-universal"
@@ -162,7 +162,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         {/* GTM noscript */}
     <noscript>
       <iframe
-        src="https://www.googletagmanager.com/ns.html?id=GTM-W2FGRMJ9"
+        src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
         height="0"
         width="0"
         style={{ display: "none", visibility: "hidden" }}
